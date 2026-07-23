37912fc72dfcceb1ec51f38e0a4d6045dec59d1f
nicolas <nicolas08lourenco@gmail.com>
2026-07-23 14:38:43 -0300

    Arquivos com extensão .php criados para funcionamento do projeto

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+// Configurações do seu roteador Mikrotik de testes
+define('MK_HOST', '192.168.1.1'); // IP do seu Mikrotik
+define('MK_USER', 'admin');        // Usuário da API
+define('MK_PASS', 'sua_senha');     // Senha
+define('MK_PORT', 8728);           // Porta padrão da API do RouterOS (8728 HTTP / 8729 HTTPS)

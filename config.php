9c9f5924d0ca8ed882a1ea4f82bcb578b66f4cac
Nicolas Lourenço <165731017+nicolaslourenc2@users.noreply.github.com>
2026-07-24 13:59:10 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-// Configurações do seu roteador Mikrotik de testes
-define('MK_HOST', '192.168.56.101'); // IP do seu Mikrotik
-define('MK_USER', 'admin');        // Usuário da API
+// Configurações do Mikrotik de testes
+define('MK_HOST', '192.168.56.101'); // IP do Mikrotik
+define('MK_USER', 'admin');        // Usuário da mikrotik
 define('MK_PASS', 'Terra_sal!222');     // Senha
 define('MK_PORT', 8728);           // Porta padrão da API do RouterOS (8728 HTTP / 8729 HTTPS)  

fd61210f06c00710110dfb74b3d0327721bf259a
nicolas <nicolas08lourenco@gmail.com>
2026-07-24 13:48:26 -0300

    incluido dados do mikrotik na vm

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Configurações do seu roteador Mikrotik de testes
-define('MK_HOST', '192.168.1.1'); // IP do seu Mikrotik
+define('MK_HOST', '192.168.56.101'); // IP do seu Mikrotik
 define('MK_USER', 'admin');        // Usuário da API
-define('MK_PASS', 'sua_senha');     // Senha
-define('MK_PORT', 8729);           // Porta padrão da API do RouterOS (8728 HTTP / 8729 HTTPS)  
+define('MK_PASS', 'Terra_sal!222');     // Senha
+define('MK_PORT', 8728);           // Porta padrão da API do RouterOS (8728 HTTP / 8729 HTTPS)  

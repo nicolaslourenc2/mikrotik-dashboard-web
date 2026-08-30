52b8a55865291f13415672c1df3bb58ab2ef56a6
Nicolas Lourenço <lourenco.n@pucpr.edu.br>
2026-08-30 10:56:29 -0300

    retirado credenciais verdadeiras do mikrotik no arquivo config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-// Configurações do seu roteador Mikrotik de testes
-define('MK_HOST', '192.168.1.113'); // IP do Mikrotik
-define('MK_USER', 'admin');        // Usuário do mikrotik
-define('MK_PASS', 'Gr@nd3P0d3r');     // Senha
+// Configurações do Mikrotik de testes
+define('MK_HOST', '127.0.0.1'); // IP do Mikrotik
+define('MK_USER', 'exemplo');        // Usuário do mikrotik
+define('MK_PASS', 'exemplo');     // Senha
 define('MK_PORT', 8728);           // Porta padrão da API do RouterOS (8728 HTTP / 8729 HTTPS)  
